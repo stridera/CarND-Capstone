3949ce412b7ccc6ae390dfcb5ce92edf61434e53
ruggero <ruleva1983@gmail.com>
2017-10-24 17:32:17 +0200

    Dataset donwloader and example notebook added

--- sandbox/ruggero/ubuntu-download-dataset.sh
+++ sandbox/ruggero/ubuntu-download-dataset.sh
@@ -2,10 +2,31 @@
 
 folder='../../data/traffic-light-models'
 
-wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_11_06_2017.tar.gz -P $folder
-tar -xzvf $folder/ssd_mobilenet_v1_coco_11_06_2017.tar.gz -C $folder/
-rm $folder/ssd_mobilenet_v1_coco_11_06_2017.tar.gz
+net1='ssd_mobilenet_v1_coco_11_06_2017.tar.gz'
+net2='ssd_inception_v2_coco_11_06_2017.tar.gz'
+net3='rfcn_resnet101_coco_11_06_2017.tar.gz'
 
-wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_11_06_2017.tar.gz -P $folder
-tar -xzvf $folder/ssd_inception_v2_coco_11_06_2017.tar.gz -C $folder/
-rm $folder/ssd_inception_v2_coco_11_06_2017.tar.gz
+
+if [ ! -d "$folder/ssd_mobilenet_v1_coco_11_06_2017" ]; then
+    wget http://download.tensorflow.org/models/object_detection/$net1 -P $folder
+    tar -xzvf $folder/$net1 -C $folder/
+    rm $folder/$net1
+else
+    echo "SSD mobilenet already downloaded...skipping..."
+fi
+
+if [ ! -d "$folder/ssd_mobilenet_v1_coco_11_06_2017" ]; then
+    wget http://download.tensorflow.org/models/object_detection/$net2 -P $folder
+    tar -xzvf $folder/$net2 -C $folder/
+    rm $folder/$net2
+else
+    echo "SSD inception already downloaded...skipping..."
+fi
+
+if [ ! -d "$folder/rfcn_resnet101_coco_11_06_2017" ]; then
+    wget http://download.tensorflow.org/models/object_detection/$net3 -P $folder
+    tar -xzvf $folder/$net3 -C $folder/
+    rm $folder/$net3
+else
+    echo "Region Based Resnet already downloaded...skipping..."
+fi
