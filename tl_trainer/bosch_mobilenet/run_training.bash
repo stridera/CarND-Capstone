22fb3987232904b04a3468bf29f733da7828ee5e
Unknown <rpicatoste@gmail.com>
2017-11-14 02:21:26 +0100

    Ready to run resnet frcnn network training.

--- tl_trainer/bosch_mobilenet/run_training.bash
+++ tl_trainer/bosch_mobilenet/run_training.bash
@@ -2,7 +2,7 @@
 
 # Generate the temp.config with the proper paths to call the training.
 temp_text='s#PATH_TO_BE_CONFIGURED#'$CURRENT_TRANSFER_LEARNING_FOLDER'#g'
-sed -e ${temp_text} ssd_mobilenet_v1_bosch.config > temp.config
+sed -e ${temp_text} ${NETWORK_TO_BE_TRAINED}.config > temp.config
 
 # Call the training function.
 python ${OBJECT_DETECTION_API_FOLDER}/research/object_detection/train.py \
--- tl_trainer/bosch_mobilenet/set_env.bash
+++ tl_trainer/bosch_mobilenet/set_env.bash
@@ -9,3 +9,7 @@
 export CURRENT_TRANSFER_LEARNING_FOLDER=/home/cricho/workspace/CarND-Capstone/tl_trainer/bosch_mobilenet
 export OBJECT_DETECTION_API_FOLDER=/home/cricho/workspace/models
 export BOSCH_DATA_FOLDER=/media/cricho/Data/machine_learning_data/carnd/bosch
+
+# Leave uncommented only the network to be used.
+export NETWORK_TO_BE_TRAINED=faster_rcnn_resnet101_bosch
+#export NETWORK_TO_BE_TRAINED=ssd_mobilenet_v1_bosch
